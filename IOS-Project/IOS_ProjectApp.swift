de99cc98b846471bbbe5c8390e0dffec9d38cfcb
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-04-26 18:01:40 -0700

    Initial Commit

---
+++ IOS-Project/IOS_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_ProjectApp.swift
+//  IOS-Project
+//
+//  Created by admin on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
